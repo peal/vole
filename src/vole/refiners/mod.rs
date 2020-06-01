00bd8da64001f8ffb15b59d4a42956157c5ee09b
Giacomo Fenzi <giacomofenzi@outlook.com>
2020-06-02 00:38:00 +0200

    Small changes

--- src/vole/refiners/mod.rs
+++ src/vole/refiners/mod.rs
@@ -7,6 +7,8 @@ use crate::trace;
 pub trait Refiner<State: super::state::State> {
     fn name(&self) -> String;
 
+    // TODO: I would like this to take an arbitrary permutation,
+    // but it conflicts with a couple of things (I think maybe a Refiner, RefinerExt pair could solve it)
     fn check(&self, p: &FullPermutation) -> bool;
 
     fn refine_begin(&mut self, _: &mut State) -> trace::Result<()> {
