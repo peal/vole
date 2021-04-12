15452f7ecdc03909ec143aca9cdf329c0fa8d225
Chris Jefferson <caj21@st-andrews.ac.uk>
2021-04-12 09:53:55 +0100

    Switch to seahash, as it has less dependancies

--- src/datastructures/hash.rs
+++ src/datastructures/hash.rs
@@ -4,14 +4,13 @@ use std::{
     hash::{Hash, Hasher},
     num::Wrapping,
 };
-use twox_hash::XxHash64;
 
 /// A simple helper to hash any object
 pub fn do_hash<T>(obj: T) -> Wrapping<usize>
 where
     T: Hash,
 {
-    let mut hasher = XxHash64::default();
+    let mut hasher = seahash::SeaHasher::default();
     obj.hash(&mut hasher);
     Wrapping(hasher.finish() as usize)
 }
