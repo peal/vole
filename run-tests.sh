51cfc5497ce87906f3645a576ff4540b98940991
Chris Jefferson <caj21@st-andrews.ac.uk>
2021-06-11 12:00:28 +0100

    Make tests work without 'parallel'

--- run-tests.sh
+++ run-tests.sh
@@ -4,8 +4,16 @@ set -euo pipefail
 
 (cd rust && cargo test --release -q)
 
-( for i in tst/*.tst; do
-        echo gap -r -q -c "'QUIT_GAP(Test(\"$i\"));'"
-    done ) | parallel --progress
+
+if which parallel 2> /dev/null; then
+    ( for i in tst/*.tst; do
+            echo gap -r -q -c "'QUIT_GAP(Test(\"$i\"));'"
+        done ) | parallel --progress
+else
+    for i in tst/*.tst; do
+        gap -r -q -c 'QUIT_GAP(Test("'$i'"));'
+    done
+fi;
+
 
 echo "Tests all passed!"
